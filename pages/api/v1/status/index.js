ec2c1db0f116a7c1cab81a4ff539aeeb31b6632d
Fernando José <fernando.tecnicoinfo7@gmail.com>
2023-11-16 10:37:58 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Alunos do curso são acima da média" });
 }
 export default status;
