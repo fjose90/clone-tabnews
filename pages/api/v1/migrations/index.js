239a50d3f841cf7546a78375b0076205b5ad32e5
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-06-12 14:45:35 -0300

    atualizações do trabalho

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,13 +1,27 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+  const defaultMigrationOptions = {
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    response.status(200).json(pendingMigrations);
+  }
+  if (request.method === "POST") {
+    const migratedMigration = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    if (migratedMigration.length > 0) {
+      response.status(201).json(migratedMigration);
+    }
+    response.status(200).json(migratedMigration);
+  }
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -11,7 +11,6 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
