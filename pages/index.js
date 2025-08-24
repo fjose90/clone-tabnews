80a7abb6617d4e69e7eaee7b399ca81d104aa81e
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-08-24 20:34:57 -0300

    feat: add initial version on of `/status` page

--- pages/index.js
+++ pages/index.js
@@ -58,3 +58,32 @@ function DatabaseStatus() {
     </>
   );
 }
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
+}
